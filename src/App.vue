e4407a9b20686bd524003de5c82076a0cc540c0d
oringht <1549698919@qq.com>
2019-04-06 20:18:12 +0800

    下班第一天打卡

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <!-- 头部 -->
     <mt-header fixed title="黑马程序员"></mt-header>
     <!-- 主体 -->
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
     <!-- 底部 -->
     <nav class="mui-bar mui-bar-tab">
       <router-link class="mui-tab-item mui-active" to="/home">
@@ -36,5 +38,19 @@ export default {};
 <style lang="scss" scoped>
 .app-container {
   padding-top: 40px;
+  overflow-x: hidden;
+}
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
